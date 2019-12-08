d6e31649e8ceac8d046490b7333011fe496c006f
MrTinGonic <leopold.wurm@fh-joanneum.at>
2019-12-08 11:01:25 +0100

    Modified first concept.

--- script/concept/scriptConcept.sh
+++ script/concept/scriptConcept.sh
@@ -3,6 +3,8 @@
 # var = first param ≈ path of .txt input
 #echo "Path: "$1
 input=./inputTmp.txt
+declare -a assArray
+declare -i lineCount=0
 
 function checkCmd(){
   command -v $1 > /dev/null
@@ -21,15 +23,32 @@ then
   checkCmd $cmd
   if [[ $? -eq 0 ]]
   then
-    # save valid command to array
-    IFS=' ' read -r -a array <<< "$line"
-    # print array
-    for i in "${array[*]}"
-    do
-      echo $i
-    done
-  printf "\n"
+  #   # save valid command to array
+  #   IFS=' ' read -r -a array <<< "$line"
+  #   # print array
+  #   for i in "${array[*]}"
+  #   do
+  #     echo $i
+  #   done
+  # printf "\n"
+    # save valid command to associative array.
+    assArray[$lineCount]="$line"
+    # echo $lineCount
+    # printf '%s\n' "${assArray[$lineCount]}"
+    ((lineCount=lineCount+1))
   fi
 fi
 # input source
 done < "$input"
+
+length=${#assArray[@]}
+echo $length
+echo First entry: ${assArray[0]}
+echo Last entry: ${assArray[(($length-1))]}
+
+
+# print associative array
+for key in "${assArray[@]}"
+do
+  printf '%s\n' "$key"
+done
